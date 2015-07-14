587236f55e44c9e83a9ea559645926c988877ae1
Klaus Holder <klaus.a.holder@gmail.com>
2015-07-14 17:18:04 -0400

    followed tutorial creating web camera interfacethat both captures image in canvas element as well as in a png file.

---
+++ profile card files/js/script.js
@@ -0,0 +1,14 @@
+// Add your Javascript effects here.
+
+window.addEventListener('load', function() {
+
+  var myCard = document.querySelector('#klausholder');
+  var flipButtons = Array.prototype.slice.call(myCard.querySelectorAll('i.flip'));
+  
+  flipButtons.forEach(function(button) {
+
+    button.addEventListener('click', function() {
+     myCard.classList.toggle('flipped');
+    });
+  });
+});
